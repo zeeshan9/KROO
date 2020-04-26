8b8cabf3667125d20d251dd8d161d5d7562f05a5
zeeshan mushtaq <zeeshanmushtaq76@gmail.com>
2020-04-27 01:33:19 +0500

    pull changes of apis and cion change to local

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Login from './src/components/screens/Login';
-import Register from './src/components/screens/Register';
-import Dashboard from './src/components/screens/Dashboard';
-import { Provider } from 'react-redux';
-import store from './src/store';
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./src/components/screens/Login";
+import Register from "./src/components/screens/Register";
+import Dashboard from "./src/components/screens/Dashboard";
+import { Provider } from "react-redux";
+import store from "./src/store";
+import ChatList from "./src/components/screens/chatcomponent/ChatList";
 
 const Stack = createStackNavigator();
 
-const title = 'KROO';
+const title = "KROO";
 
 const App = () => {
   return (
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
   // apiKey: "AIzaSyD7fhHwPtq6B0rkxvuyaDYsHFskg3igEJk",
@@ -10,13 +10,13 @@ const firebaseConfig = {
   // storageBucket: "kroo-c9025.appspot.com",
   // messagingSenderId: "464171276433",
   // appId: "1:464171276433:web:f6fdcfa641bddc6b670730",
-  apiKey: 'AIzaSyD2wHF-tTXamc6-54cL0ZAq1O5aP0VyJsI',
-  authDomain: 'kroo-d99da.firebaseapp.com',
-  databaseURL: 'https://kroo-d99da.firebaseio.com',
-  projectId: 'kroo-d99da',
-  storageBucket: 'kroo-d99da.appspot.com',
-  messagingSenderId: '576786370685',
-  appId: '1:576786370685:web:b862ee319352ea98c72114',
+  apiKey: "AIzaSyD2wHF-tTXamc6-54cL0ZAq1O5aP0VyJsI",
+  authDomain: "kroo-d99da.firebaseapp.com",
+  databaseURL: "https://kroo-d99da.firebaseio.com",
+  projectId: "kroo-d99da",
+  storageBucket: "kroo-d99da.appspot.com",
+  messagingSenderId: "576786370685",
+  appId: "1:576786370685:web:b862ee319352ea98c72114",
 };
 
 firebase.initializeApp(firebaseConfig);
