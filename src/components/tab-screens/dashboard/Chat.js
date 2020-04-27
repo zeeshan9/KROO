ed52ac00e8057593a5c668f12b59d998176f3f05
zeeshan mushtaq <zeeshanmushtaq76@gmail.com>
2020-04-27 15:15:00 +0500

    floating button

--- src/components/tab-screens/dashboard/Chat.js
+++ src/components/tab-screens/dashboard/Chat.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import Colors from "../../../constants/colors";
 import { CoinBar } from "../../screens/homecomponents/CoinBar";
 import ChatGroups from "../../screens/chatcomponent/ChatGroups";
 import { Ranking } from "../../screens/homecomponents/Ranking";
 
 const Chat = ({ auth, navigation }) => {
+  const SampleFunction = () => {
+    Alert.alert("Floating Button Clicked");
+  };
+
   return (
     <View style={styles.container}>
       {/* <View style={styles.topContainer}>
@@ -17,6 +28,19 @@ const Chat = ({ auth, navigation }) => {
       <View style={styles.bottomcontainer}>
         <Ranking />
       </View>
+      <TouchableOpacity
+        activeOpacity={0.5}
+        onPress={this.SampleFunction}
+        style={styles.TouchableOpacityStyle}
+      >
+        <Image
+          source={{
+            uri:
+              "https://reactnativecode.com/wp-content/uploads/2017/11/Floating_Button.png",
+          }}
+          style={styles.FloatingButtonStyle}
+        />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -42,6 +66,21 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     paddingVertical: 3,
+  },
+  // FLoating action button style
+  TouchableOpacityStyle: {
+    position: "absolute",
+    width: 50,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    right: 30,
+    bottom: 30,
+  },
+  FloatingButtonStyle: {
+    resizeMode: "contain",
+    width: 50,
+    height: 50,
   },
 });
 
