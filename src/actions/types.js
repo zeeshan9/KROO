b07d02fdfbb31418ced501807cfc09b303dd3a19
ssssyed98 <ssssyed98@gmail.com>
2020-05-01 20:05:31 +0500

    Updated files

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,32 +1,33 @@
 // Server address
-export const SERVER_URL = "http://192.168.100.52:5000";
+// export const SERVER_URL = "http://192.168.100.52:5000";
+export const SERVER_URL = 'https://still-citadel-35667.herokuapp.com";
 // export const SERVER_URL = 'http://192.168.1.13:5000';
 
 // User
-export const REGISTRATION_SUCCESSSFUL = "REGISTRATION_SUCCESSSFUL";
-export const LOGIN_SUCCESSSFUL = "LOGIN_SUCCESSSFUL";
-export const USER_LOADED = "USER_LOADED";
-export const USER_ERROR = "USER_ERROR";
-export const USER_RANKING_LOADED = "USER_RANKING_LOADED";
+export const REGISTRATION_SUCCESSSFUL = 'REGISTRATION_SUCCESSSFUL';
+export const LOGIN_SUCCESSSFUL = 'LOGIN_SUCCESSSFUL';
+export const USER_LOADED = 'USER_LOADED';
+export const USER_ERROR = 'USER_ERROR';
+export const USER_RANKING_LOADED = 'USER_RANKING_LOADED';
 
 // Kroo
-export const KROO_ADDED_SUCCESSFULLY = "KROO_ADDED_SUCCESSFULLY";
-export const ALL_KROOS_LOADED = "ALL_KROO_LOADED";
-export const KROO_ERROR = "KROO_ERROR";
-export const KROO_MEMBER_ADDED = "KROO_MEMBER_ADDED";
+export const KROO_ADDED_SUCCESSFULLY = 'KROO_ADDED_SUCCESSFULLY';
+export const ALL_KROOS_LOADED = 'ALL_KROO_LOADED';
+export const KROO_ERROR = 'KROO_ERROR';
+export const KROO_MEMBER_ADDED = 'KROO_MEMBER_ADDED';
 
 // Messages
-export const MESSAGE_SENT = "MESSAGE_SENT";
-export const ALL_MESSAGES_LOADED = "ALL_MESSAGES_LOADED";
-export const MESSAGE_ERROR = "MESSAGE_ERROR";
+export const MESSAGE_SENT = 'MESSAGE_SENT';
+export const ALL_MESSAGES_LOADED = 'ALL_MESSAGES_LOADED';
+export const MESSAGE_ERROR = 'MESSAGE_ERROR';
 
 //User
 
-export const ALL_USER_LOADED = "ALL_USER_LOADED";
-export const USER_MSG_ERROR = "USER_MSG_ERROR";
+export const ALL_USER_LOADED = 'ALL_USER_LOADED';
+export const USER_MSG_ERROR = 'USER_MSG_ERROR';
 
 // User Messages
-export const USER_MSG_LOADED = "USER_MSG_LOADED";
+export const USER_MSG_LOADED = 'USER_MSG_LOADED';
 
 // Ranking
-export const ALL_RANKING_LOADED = "ALL_RANKING_LOADED";
+export const ALL_RANKING_LOADED = 'ALL_RANKING_LOADED';
