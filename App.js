5430a078f2272273b2ad851b2a2226886611a688
zeeshan mushtaq <zeeshanmushtaq76@gmail.com>
2020-05-01 20:15:11 +0500

    commit and

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./src/components/screens/Login";
 import Register from "./src/components/screens/Register";
-import TermsAndConditions from "./src/components/screens/TermsAndConditions";
+// import TermsAndConditions from "./src/components/screens/TermsAndConditions";
 import Dashboard from "./src/components/screens/Dashboard";
+import store from "./src/store";
+import { Provider } from "react-redux";
 
 const Stack = createStackNavigator();
 
@@ -13,26 +15,28 @@ const title = "KROO";
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen name='Login' component={Login} options={{ title }} />
-        <Stack.Screen
-          name='Register'
-          component={Register}
-          options={{ title }}
-        />
-        <Stack.Screen
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Login'>
+          <Stack.Screen name='Login' component={Login} options={{ title }} />
+          <Stack.Screen
+            name='Register'
+            component={Register}
+            options={{ title }}
+          />
+          {/* <Stack.Screen
           name='TermsAndConditions'
           component={TermsAndConditions}
           options={{ title }}
-        />
-        <Stack.Screen
-          name='Dashboard'
-          component={Dashboard}
-          options={{ title, headerLeft: null }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+        /> */}
+          <Stack.Screen
+            name='Dashboard'
+            component={Dashboard}
+            options={{ title, headerLeft: null }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
